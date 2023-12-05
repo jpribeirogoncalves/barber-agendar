14c431d585a80fa17bb82df9d58979a7f4f30e24
João Pedro Ribeiro Gonçalves <joaopedroribeirogoncalves@gmail.com>
2023-12-05 00:16:21 -0300

    att href="infex.php"

--- agendar.php
+++ agendar.php
@@ -20,7 +20,7 @@ unset($_GET['error']);
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-          <a class="navbar-brand" href="index.html">Barbearia Garagem</a>
+          <a class="navbar-brand" href="index.php">Barbearia Garagem</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -28,7 +28,7 @@ unset($_GET['error']);
           <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.html">Início</a>
+                        <a class="nav-link" href="index.php">Início</a>
                     </li>
                 </ul>
           </div>
